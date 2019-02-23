1cd26eb0c49be35b277842ab97dea026ebd5903d
Michael Redig <mredig@gmail.com>
2019-02-23 13:18:19 -0600

    Initial Commit

---
+++ CSV Generator/CSV_Generator.h
@@ -0,0 +1,19 @@
+//
+//  CSV_Generator.h
+//  CSV Generator
+//
+//  Created by Michael Redig on 2/23/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CSV_Generator.
+FOUNDATION_EXPORT double CSV_GeneratorVersionNumber;
+
+//! Project version string for CSV_Generator.
+FOUNDATION_EXPORT const unsigned char CSV_GeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSV_Generator/PublicHeader.h>
+
+
